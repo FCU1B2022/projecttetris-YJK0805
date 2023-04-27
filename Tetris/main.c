0478662bdc983703d141dcdb711567bc2d16b821
YJK0805 <jerryko103@gmail.com>
2023-04-27 09:58:40 +0800

    create Color,ShapeId

--- Tetris/main.c
+++ Tetris/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+typedef enum{
+	RED = 41,
+	GREEN,YELLOW,BLUE,PURPLE,CYAN,WHITE,BLACK = 0,
+}Color;
+typedef enum {
+	EMPTY = -1, I, J, L, O, S, T, Z,
+}ShapeId;
+typedef struct {
+	Color color;
+	ShapeId shape;
+};
 int main(){
-	printf("Hello, World!\n");
 	return 0;
 }
